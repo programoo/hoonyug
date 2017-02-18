d4a63390864b27d075203cf72d80fd213fcf0f43
programoo <prakarnwongsanit@gmail.com>
2017-02-18 17:17:01 +0700

    Add all neccessary data

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170218094752) do
+ActiveRecord::Schema.define(version: 20170218101422) do
 
   create_table "markets", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,30 @@ ActiveRecord::Schema.define(version: 20170218094752) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "stock_data", force: :cascade do |t|
+    t.string   "date"
+    t.decimal  "assets",          precision: 15, scale: 3
+    t.decimal  "liabilities",     precision: 15, scale: 3
+    t.decimal  "equity",          precision: 15, scale: 3
+    t.decimal  "paid_up_capital", precision: 15, scale: 3
+    t.decimal  "revenue",         precision: 15, scale: 3
+    t.decimal  "net_profit",      precision: 15, scale: 3
+    t.decimal  "eps",             precision: 15, scale: 3
+    t.decimal  "roa",             precision: 15, scale: 3
+    t.decimal  "roe",             precision: 15, scale: 3
+    t.decimal  "last_price",      precision: 15, scale: 3
+    t.decimal  "market_cap",      precision: 15, scale: 3
+    t.decimal  "pe",              precision: 15, scale: 3
+    t.decimal  "pbv",             precision: 15, scale: 3
+    t.decimal  "book_value",      precision: 15, scale: 3
+    t.decimal  "dividend_yield",  precision: 15, scale: 3
+    t.datetime "created_at",                               null: false
+    t.datetime "updated_at",                               null: false
+    t.integer  "stock_id"
+  end
+
+  add_index "stock_data", ["stock_id"], name: "index_stock_data_on_stock_id"
 
   create_table "stocks", force: :cascade do |t|
     t.string   "name"
